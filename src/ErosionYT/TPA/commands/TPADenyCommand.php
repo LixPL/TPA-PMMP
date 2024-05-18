c065b204ad76056657f8688048583a065dec11d7
Lixツ <ktostamhip@gmail.com>
2024-05-18 14:55:57 +0200

    Update TPADenyCommand.php

--- src/ErosionYT/TPA/commands/TPADenyCommand.php
+++ src/ErosionYT/TPA/commands/TPADenyCommand.php
@@ -10,20 +10,27 @@ use ErosionYT\TPA\TPA;
 
 class TPADenyCommand extends Command {
 
+    private $owner;
+
     public function __construct(TPA $owner){
-        parent::__construct("tpadeny");
+        parent::__construct("tpadeny", $owner->getConfig()->get("tpadenyCommandDescription"), "/tpadeny");
         $this->owner = $owner;
-        $this->setDescription($this->owner->getConfig()->get("tpadenyCommandDescription"));
         $this->setPermission("tpa.command");
     }
 
     public function execute(CommandSender $player, string $commandLabel, array $args): bool{
+        if(!$this->testPermission($player)){
+            return true;
+        }
+
         if($player instanceof Player){
             if($this->owner->hasRequest($player)){
                 $this->owner->denyTPARequest($player);
             }else{
                 $player->sendMessage("§6» §7You do not have any teleport requests");
             }
+        } else {
+            $player->sendMessage("§cThis command can only be used in-game");
         }
         return true;
     }
