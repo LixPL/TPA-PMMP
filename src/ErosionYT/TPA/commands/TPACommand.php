de93dcb70f0d7b7014556160e8d3b4718f301262
Lixツ <ktostamhip@gmail.com>
2024-05-18 14:54:48 +0200

    Update TPACommand.php

--- src/ErosionYT/TPA/commands/TPACommand.php
+++ src/ErosionYT/TPA/commands/TPACommand.php
@@ -10,14 +10,19 @@ use ErosionYT\TPA\TPA;
 
 class TPACommand extends Command {
 
+    private $owner;
+
     public function __construct(TPA $owner){
-        parent::__construct("tpa");
+        parent::__construct("tpa", $owner->getConfig()->get("tpaCommandDescription"), "/tpa <player>");
         $this->owner = $owner;
-        $this->setDescription($this->owner->getConfig()->get("tpaCommandDescription"));
         $this->setPermission("tpa.command");
     }
 
     public function execute(CommandSender $player, string $commandLabel, array $args): bool{
+        if(!$this->testPermission($player)){
+            return true;
+        }
+
         if($player instanceof Player){
             if(isset($args[0])){
                 if(($target = $this->owner->getServer()->getPlayerByPrefix($args[0])) !== null){
@@ -27,7 +32,11 @@ class TPACommand extends Command {
                 }else{
                     $player->sendMessage("§cThat player cannot be found");
                 }
+            } else {
+                $player->sendMessage("§cPlease specify a player");
             }
+        } else {
+            $player->sendMessage("§cThis command can only be used in-game");
         }
         return true;
     }
